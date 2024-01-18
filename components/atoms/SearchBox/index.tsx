25d7b96314b233178cf89dda16ced5404671fbc3
Johan Bejar <johanbejarc@hotmail.com>
2024-01-18 01:32:29 -0500

    added reset search, removed api route

--- components/atoms/SearchBox/index.tsx
+++ components/atoms/SearchBox/index.tsx
@@ -32,6 +32,24 @@ export const SearchBox = connectWithQuery(({ currentRefinement, refine }) => {
       onChange={(e) => refine(e.currentTarget.value)}
       label="Busca tu evento..."
       size="md"
+      icon={
+        <div className="hover:cursor-pointer" onClick={() => refine("")}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="w-4 h-4"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M6 18 18 6M6 6l12 12"
+            />
+          </svg>
+        </div>
+      }
     />
   );
 });
