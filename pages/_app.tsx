3e76525aa38e2f492d98e5cc48495134bbf1d006
Johan Bejar <johanbejarc@hotmail.com>
2024-02-01 10:40:02 -0500

    installed ga4

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,11 @@
 import "@/styles/globals.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div>
+      <GoogleAnalytics gaId="G-2RF08LDP99" />
       <Component {...pageProps} />
     </div>
   );
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,9 @@
-import { GoogleTagManager } from "@next/third-parties/google";
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <GoogleTagManager gtmId="G-2RF08LDP99" />
         <title>Eventos Peru</title>
       </Head>
       <body>
